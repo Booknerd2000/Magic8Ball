9558f2825bb7ec30416c25fcc85e446bd1c3846a
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-22 08:03:05 -0500

    Added question and ten if statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,9 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 	Scanner keyboard;
+	keyboard = new Scanner(System.in);
 
+	System.out.println("think of a question and type a number");
+	int userNumber = keyboard.nextInt();
 
-
-
+        if(userNumber>=1&&userNumber<=10) {
+            System.out.println("");
+        }
+        if(userNumber>=11&&userNumber<=20) {
+            System.out.println("");
+        }
+        if(userNumber>=21&&userNumber<=30) {
+            System.out.println("");
+        }
+        if(userNumber>=31&&userNumber<=40) {
+            System.out.println("");
+        }
+        if(userNumber>=41&&userNumber<=50) {
+            System.out.println("");
+        }
+        if(userNumber>=51&&userNumber<=60) {
+            System.out.println("");
+        }
+        if(userNumber>=61&&userNumber<=70) {
+            System.out.println("");
+        }
+        if(userNumber>=71&&userNumber<=80) {
+            System.out.println("");
+        }
+        if(userNumber>=81&&userNumber<=90) {
+            System.out.println("");
+        }
+        if(userNumber>=91&&userNumber<=100) {
+            System.out.println("");
+        }
     }
 }
