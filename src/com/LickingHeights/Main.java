a0dac50fd91ea2ba849581e8ae825cb208d13486
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-22 07:32:03 -0500

    Added user input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,12 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner keyboard;
+
+
+
+
     }
 }
