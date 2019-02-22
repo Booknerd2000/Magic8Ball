f884cf60980c989418499bf2ee4b0e43d2164349
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-22 08:10:36 -0500

    Added Responses

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,34 +10,35 @@ public class Main {
 	int userNumber = keyboard.nextInt();
 
         if(userNumber>=1&&userNumber<=10) {
-            System.out.println("");
+            System.out.println("OUT LOOK GOOD");
         }
         if(userNumber>=11&&userNumber<=20) {
-            System.out.println("");
+            System.out.println("ASK AGAIN LATER");
         }
         if(userNumber>=21&&userNumber<=30) {
-            System.out.println("");
+            System.out.println("YES DEFINITELY");
         }
         if(userNumber>=31&&userNumber<=40) {
-            System.out.println("");
+            System.out.println("NO");
         }
         if(userNumber>=41&&userNumber<=50) {
-            System.out.println("");
+            System.out.println("DON'T COUNT ON IT");
         }
         if(userNumber>=51&&userNumber<=60) {
-            System.out.println("");
+            System.out.println("ABSOLUTELY");
         }
         if(userNumber>=61&&userNumber<=70) {
-            System.out.println("");
+            System.out.println("MOST LIKELY");
         }
         if(userNumber>=71&&userNumber<=80) {
-            System.out.println("");
+            System.out.println("GO FOR IT");
         }
         if(userNumber>=81&&userNumber<=90) {
-            System.out.println("");
+            System.out.println("CONCENTRATE AND ASK AGAIN");
         }
         if(userNumber>=91&&userNumber<=100) {
-            System.out.println("");
+            System.out.println("MY SOURCES SAY NO");
         }
+
     }
 }
