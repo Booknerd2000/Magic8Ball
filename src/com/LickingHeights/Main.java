b3e042c9a2170a0b3c90d26a785cd7801c088c38
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-26 07:28:13 -0500

    Added an else to adjust the code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -19,37 +19,37 @@ public class Main {
         System.out.println("       ........       .....       ");
         System.out.println("            ..........            ");
 
-	System.out.println("Think of a question and type a number for a response");
+	System.out.println("Think of a question and type a number for a response.");
 	int userNumber = keyboard.nextInt();
 
         if(userNumber>=1&&userNumber<=10) {
             System.out.println("OUTLOOK GOOD");
         }
-        if(userNumber>=11&&userNumber<=20) {
+        else if(userNumber>=11&&userNumber<=20) {
             System.out.println("ASK AGAIN LATER");
         }
-        if(userNumber>=21&&userNumber<=30) {
+        else if(userNumber>=21&&userNumber<=30) {
             System.out.println("YES DEFINITELY");
         }
-        if(userNumber>=31&&userNumber<=40) {
+        else if(userNumber>=31&&userNumber<=40) {
             System.out.println("NO");
         }
-        if(userNumber>=41&&userNumber<=50) {
+        else if(userNumber>=41&&userNumber<=50) {
             System.out.println("DON'T COUNT ON IT");
         }
-        if(userNumber>=51&&userNumber<=60) {
+        else if(userNumber>=51&&userNumber<=60) {
             System.out.println("ABSOLUTELY");
         }
-        if(userNumber>=61&&userNumber<=70) {
+        else if(userNumber>=61&&userNumber<=70) {
             System.out.println("MOST LIKELY");
         }
-        if(userNumber>=71&&userNumber<=80) {
+        else if(userNumber>=71&&userNumber<=80) {
             System.out.println("GO FOR IT");
         }
-        if(userNumber>=81&&userNumber<=90) {
+        else if(userNumber>=81&&userNumber<=90) {
             System.out.println("CONCENTRATE AND ASK AGAIN");
         }
-        if(userNumber>=91&&userNumber<=100) {
+        else if(userNumber>=91&&userNumber<=100) {
             System.out.println("MY SOURCES SAY NO");
         }
         else{
