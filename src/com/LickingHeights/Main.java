88e0868b6687af2f778e569584a89b4f0fa5455c
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-26 07:55:45 -0500

    Added a Magic 8 ball border around responses

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -22,35 +22,81 @@ public class Main {
 	System.out.println("Think of a question and type a number for a response.");
 	int userNumber = keyboard.nextInt();
 
+        System.out.println("            ..........            ");
+        System.out.println("        ..................        ");
+        System.out.println("      ..........    ........      ");
+        System.out.println("     .......           ......     ");
+        System.out.println("    .......             ......    ");
+
         if(userNumber>=1&&userNumber<=10) {
-            System.out.println("OUTLOOK GOOD");
+            System.out.println("    ...... OUTLOOK GOOD  .....    ");
+            System.out.println("    .......              .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=11&&userNumber<=20) {
-            System.out.println("ASK AGAIN LATER");
+            System.out.println("    ......    ASK AGAIN  .....    ");
+            System.out.println("    .......     LATER    .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=21&&userNumber<=30) {
-            System.out.println("YES DEFINITELY");
+            System.out.println("    ...... YES DEFINITELY.....    ");
+            System.out.println("    .......              .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=31&&userNumber<=40) {
-            System.out.println("NO");
+            System.out.println("    ......       NO      .....    ");
+            System.out.println("    .......              .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=41&&userNumber<=50) {
-            System.out.println("DON'T COUNT ON IT");
+            System.out.println("    ......   DON'T COUNT .....    ");
+            System.out.println("    .......     ON IT    .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=51&&userNumber<=60) {
-            System.out.println("ABSOLUTELY");
+            System.out.println("    ......   ABSOLUTELY  .....    ");
+            System.out.println("    .......              .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=61&&userNumber<=70) {
-            System.out.println("MOST LIKELY");
+            System.out.println("    ......   MOST LIKELY .....    ");
+            System.out.println("    .......              .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=71&&userNumber<=80) {
-            System.out.println("GO FOR IT");
+            System.out.println("    ......   GO FOR IT   .....    ");
+            System.out.println("    .......              .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=81&&userNumber<=90) {
-            System.out.println("CONCENTRATE AND ASK AGAIN");
+            System.out.println("    ......  CONCENTRATE  .....    ");
+            System.out.println("    .......      AND     .....    ");
+            System.out.println("     ....... ASK AGAIN  .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else if(userNumber>=91&&userNumber<=100) {
-            System.out.println("MY SOURCES SAY NO");
+            System.out.println("    ......   MY SOURCES  .....    ");
+            System.out.println("    .......    SAY NO    .....    ");
+            System.out.println("     .......            .....     ");
+            System.out.println("       ..........    ......       ");
+            System.out.println("            ..........            ");
         }
         else{
             System.out.println("Your number is insufficient. Make sure its a number between 1 and 100.");
